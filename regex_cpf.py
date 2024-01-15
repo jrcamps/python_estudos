2c89f9c11f34fc403e3bbe8302077ed71dae77d7
Elias Campos <juninhocampos97@gmail.com>
2024-01-15 16:23:43 -0300

    Adicionando padroes

--- regex_cpf.py
+++ regex_cpf.py
@@ -1,3 +1,14 @@
+"""
+Caracteres:	    Descrição:	                                                     Exemplos:
+[]	            Define um range ou um grupo de caracteres	                     [0-9]; [a-z]; [abc]
+*	            Marca nenhuma, uma ou mais ocorrências	                         sol*
+{}	            Quantidade de repetições de uma ocorrência definida	             [abc]{5}
+\d	            Qualquer número de 0 até 9	                                     \d{3,4}
+\w	            Qualquer número de a té 9 letra de a até z ou _	                 \w{10}
+|	            Um ou outro	                                                     @|$
+()	            Captura e agrupa	                                             (\w{4})
+"""
+
 import re
 
 padrao = 'xxx.xxx.xxx-xx'
