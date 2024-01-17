201879e2e68c86be0ae1d7e2b2a4b6b2887de895
Elias Campos <juninhocampos97@gmail.com>
2024-01-17 12:19:30 -0300

    formatando datas

--- datas.py
+++ datas.py
@@ -4,18 +4,27 @@ class DataCadastro:
     def __init__(self):
         self.momento_cadastro = datetime.today() #Pegando a data/hora atuais
 
-    def mes_atual(self):
+    def __str__(self):
+        return self.formatar_data()
+
+    def get_mes_atual(self):
         meses = ['janeiro','fevereiro','marco','abril','maio','junho','julho','agosto','setembro','outubro', 'novembro', 'dezembro']
         indice_mes = self.momento_cadastro.month - 1
         return meses[indice_mes]
     
-    def dia_da_semana(self):
+    def get_dia_da_semana(self):
         dia_da_semana = ['segunda-feira','terca-feira','quarta-feira','quinta-feira','sexta-feira','sabado','domingo']
         indice_dia = self.momento_cadastro.weekday()
         return dia_da_semana[indice_dia]
-
+    
+    def formatar_data(self):
+        data_formatada = self.momento_cadastro.strftime('%d/%m/%Y %H:%M')
+        return data_formatada
+    
 #TESTE
 cadastro = DataCadastro()
 print(cadastro.momento_cadastro)
-print(cadastro.mes_atual())
-print(cadastro.dia_da_semana())
+print(cadastro.get_mes_atual())
+print(cadastro.get_dia_da_semana())
+# print(cadastro.formatar_data())
+print(cadastro)
